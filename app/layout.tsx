5a533cef2f65152290336d7f79a674074f4fa4c2
shuhei tanaka <67859510+tnkshuuhei@users.noreply.github.com>
2023-10-02 12:50:59 +0900

    added shadcn components

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Providers } from "./providers";
-import Header from "./components/Header";
+import Header from "../components/Header";
 const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Create Next App",
